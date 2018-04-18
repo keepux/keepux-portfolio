cae4784ff19718b6b9a7f0a5bcaec944e9fc94a6
Michael Neal <michaelneal@Michaels-MacBook-Air.local>
2018-04-18 13:59:17 -0500

    interactive share

--- src/pages/interaction-design.js
+++ src/pages/interaction-design.js
@@ -3,6 +3,7 @@ import Link from 'gatsby-link';
 import interactionDesignImageOne from '../images/interaction-design/interaction-launch.png';
 import interactionDesignImageTwo from '../images/interaction-design/interaction-signin.png';
 import interactionDesignImageThree from '../images/interaction-design/interaction-admin-signin.png';
+import interactionDesignImageFour from '../images/interaction-design/interaction-share.png';
 
 const interactionDesign = () => (
     <div className="text-center">
@@ -17,6 +18,9 @@ const interactionDesign = () => (
             src={interactionDesignImageThree}
             className="block mx-auto w-1/1"
         />
+        <br />
+        <h2 className="bg-yellow inline-block my-8 p-3">Share App</h2>
+        <img src={interactionDesignImageFour} className="block mx-auto w-1/1" />
         <br />
     </div>
 );
