98567d1eb9f1a1d0d8447586835dc7d38abc8052
Michael Neal <michaelneal@Michaels-MacBook-Air.local>
2018-04-18 13:27:07 -0500

    interactive content

--- src/pages/interaction-design.js
+++ src/pages/interaction-design.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'gatsby-link';
 import interactionDesignImageOne from '../images/interaction-design/interaction-launch.png';
 import interactionDesignImageTwo from '../images/interaction-design/interaction-signin.png';
+import interactionDesignImageThree from '../images/interaction-design/interaction-admin-signin.png';
 
 const interactionDesign = () => (
     <div className="text-center">
@@ -10,6 +11,12 @@ const interactionDesign = () => (
         <br />
         <h2 className="bg-yellow inline-block my-8 p-3">Sign-In</h2>
         <img src={interactionDesignImageTwo} className="block mx-auto w-1/1" />
+        <br />
+        <h2 className="bg-yellow inline-block my-8 p-3">Admin Sign-In</h2>
+        <img
+            src={interactionDesignImageThree}
+            className="block mx-auto w-1/1"
+        />
         <br />
     </div>
 );
