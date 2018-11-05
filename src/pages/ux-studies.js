d7593b16df03779fffb1a1b8b2c58729a82742cd
Michael Neal <iammichaelneal@gmail.com>
2018-11-05 14:47:05 -0600

    optimize images

--- src/pages/ux-studies.js
+++ src/pages/ux-studies.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import uxStudiesImageOne from '../images/ux-studies/service-blueprint.png';
-import uxStudiesImageTwo from '../images/ux-studies/ux-dev-infographic.png';
 
 const uxStudies = () => (
     <div className="text-center">
@@ -22,12 +21,6 @@ const uxStudies = () => (
             </a>
         </div>
         <br />
-        <div className="shadow-md bg-white pt-6">
-            <img src={uxStudiesImageTwo} className="block mx-auto w-4/5" />
-            <h2 className="bg-yellow inline-block my-8 p-3">
-                UX Development Workflow
-            </h2>
-        </div>
     </div>
 );
 
